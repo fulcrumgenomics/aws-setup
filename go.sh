ae72b975cc91106b669a975daaf93fbc6cbaac95
Yossi Farjoun <yossi@fulcrumgenomics.com>
2025-11-18 12:58:06 -0500

    add repo cloning and default pixi config

--- go.sh
+++ go.sh
@@ -6,11 +6,25 @@ set -euo pipefail
 sudo yum update -y
 sudo yum install git git-lfs htop -y 
 
-# install pixi
+echo "Cloning repo..."
+git clone https://github.com/fulcrumgenomics/aws-setup.git
+
+echo "Installing pixi..."
 wget -qO- https://pixi.sh/install.sh | sh
 
-# install uv
+if [ ! -f "~/.pixi/config.toml" ]; then
+	echo "copying default pixi.toml to ~/.pixi/ "
+	cp aws-setup/resources/pixi.defaults.toml  ~/.pixi/config.toml
+else
+	echo "~/.pixi/config.toml already exists. NOT overwriting."
+fi 
+
+# check that the default configuration is correct
+pixi config ls
+
+echo "Installing uv..."
 wget -qO- https://astral.sh/uv/install.sh | sh
 
 # tell the user to restart the shell
 echo '======== You need to restart the shell in order for pixi to be available ========'
+
