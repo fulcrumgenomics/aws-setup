c7b2804b2061d8b1149a574570030808449be43e
Yossi Farjoun <yossi@fulcrumgenomics.com>
2025-11-17 23:17:50 -0500

    add comment in the end.

--- go.sh
+++ go.sh
@@ -13,3 +13,4 @@ wget -qO- https://pixi.sh/install.sh | sh
 wget -qO- https://astral.sh/uv/install.sh | sh
 
 
+echo '======== You need to restart the shell in order for pixi to be available ========'
